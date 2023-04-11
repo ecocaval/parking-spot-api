bef4ddfb9a41d1ccd4d85a1f3c9044bf4e2d2617
erico <ecocaval@hotmail.com>
2023-04-11 10:40:19 -0300

    fix: imports

--- src/main/java/com/api/parkingcontrol/repositories/ParkingSpotRepository.java
+++ src/main/java/com/api/parkingcontrol/repositories/ParkingSpotRepository.java
@@ -1,13 +1,9 @@
 package com.api.parkingcontrol.repositories;
 
 import com.api.parkingcontrol.models.ParkingSpotModel;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-import java.util.Optional;
 import java.util.UUID;
 
 @Repository
